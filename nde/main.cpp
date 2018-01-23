8e28044f13d1f92ff14b619f92add9892e8c97ee
JackKiefer <jack.c.kiefer@gmail.com>
2018-01-22 19:45:54 -0700

    Hope this doesn't break anything

--- nde/main.cpp
+++ nde/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include "nde.hpp"
+#include <iostream>
 
 int main()
 {
-  std::cout << nde::logistic(2.0,1.4,2.0,10.0) << std::endl;
-  std::cout << nde::solcc(1.0,0.0,2.0,3.0,-2.0,3.0).value() << std::endl;
+  std::cout << nde::logistic(2.0, 1.4, 2.0, 10.0) << std::endl;
+  std::cout << nde::solcc(1.0, 0.0, 2.0, 3.0, -2.0, 3.0).value() << std::endl;
 }
--- nde/nde.hpp
+++ nde/nde.hpp
@@ -5,12 +5,12 @@
 #include <complex>
 #include <optional>
 
-namespace nde {
+namespace nde
+{
 
 double maceps()
 {
-  typedef union
-  {
+  typedef union {
     long long ll;
     double d;
   } data;
@@ -24,55 +24,51 @@ double maceps()
 double const h_DIF = 0.00001;
 
 template <typename F, typename T>
-auto firstFiniteDif(F const & y, T t)
+auto firstFiniteDif(F const& y, T t)
 {
-  return ( y(t+h_DIF) - y(t-h_DIF) ) / (2.0*h_DIF);
+  return (y(t + h_DIF) - y(t - h_DIF)) / (2.0 * h_DIF);
 }
 
 template <typename F, typename T>
-auto secondFiniteDif(F const & y, T t)
+auto secondFiniteDif(F const& y, T t)
 {
-  return ( y(t+h_DIF) - 2.0*y(t) + y(t-h_DIF) ) / (h_DIF * h_DIF);
+  return (y(t + h_DIF) - 2.0 * y(t) + y(t - h_DIF)) / (h_DIF * h_DIF);
 }
 
 template <typename T>
-T absoluteError(T const & expectedVal, T const & measuredVal)
+T absoluteError(T const& expectedVal, T const& measuredVal)
 {
   return expectedVal - measuredVal;
 }
 
 template <typename T>
-T relativeError(T const & expectedVal, T const & measuredVal)
+T relativeError(T const& expectedVal, T const& measuredVal)
 {
   return absoluteError(expectedVal, measuredVal) / expectedVal;
 }
 
-template 
-< typename Time, 
-  typename Num, 
-  typename Param1, 
-  typename Param2
-  >
-Num logistic(Num const & p0, Param1 const & a, Param2 const & b, Time const & t)
+template <typename Time,
+          typename Num,
+          typename Param1,
+          typename Param2>
+Num logistic(Num const& p0, Param1 const& a, Param2 const& b, Time const& t)
 {
-  return a / ( ( (a/p0) - b ) * exp((-a)*t) + b );
+  return a / (((a / p0) - b) * exp((-a) * t) + b);
 }
 
 /* Second-Order, Linear, Constant Coefficient:
  * ay'' + by' + cy = f(t) */
-template
-< typename Time,
-  typename Num,
-  typename Param1,
-  typename Param2,
-  typename Param3
-  >
-std::optional<std::complex<Num>> solcc(Num y0, Num v0, Param1 a, Param2 b, Param3 c, Time t)
+template <typename Time,
+          typename Num,
+          typename Param1,
+          typename Param2,
+          typename Param3>
+std::optional<std::complex<Num>> solcc(Num const& y0, Num const& v0, Param1 const& a, Param2 const& b, Param3 const& c, Time const& t)
 {
   /* Characteristic roots from quadratic formula */
-  std::complex<Num> const radical = (b*b)-(4.0*a*c);
-  auto const r1 = (-b + sqrt(radical))/(2.0*a);
-  auto const r2 = (-b - sqrt(radical))/(2.0*a);
+  std::complex<Num> const radical = (b * b) - (4.0 * a * c);
+  auto const r1 = (-b + sqrt(radical)) / (2.0 * a);
+  auto const r2 = (-b - sqrt(radical)) / (2.0 * a);
 
   /* No solution if roots are the same */
   if (r1 == r2)
@@ -80,9 +76,9 @@ std::optional<std::complex<Num>> solcc(Num y0, Num v0, Param1 a, Param2 b, Param
     return {};
   }
 
-  auto const c1 = ( v0-(r2*y0) ) / ( r1-r2 );
-  auto const c2 = ( (r1*y0)-v0 ) / ( r1-r2 );
-  return c1*exp(r1*t) + c2*exp(r2*t);
+  auto const c1 = (v0 - (r2 * y0)) / (r1 - r2);
+  auto const c2 = ((r1 * y0) - v0) / (r1 - r2);
+  return c1 * exp(r1 * t) + c2 * exp(r2 * t);
 }
 
 } // namespace nde
